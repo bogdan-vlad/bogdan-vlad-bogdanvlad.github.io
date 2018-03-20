f1e0b38f538e14e82357f90329cf331abfd92d9d
Your name <your email address>
2018-03-20 15:46:43 +0000

    Unit test added

---
+++ Unit tests/spec/quizSpec.js
@@ -0,0 +1,16 @@
+describe("myquiz", function() {
+	it("Should select the answer", function(){
+		expect(true).toBe(true);
+	});
+});
+describe("buildQuiz", function() {
+	it("Should show quiz", function() {
+		expect(true).toBe(true);
+	});
+});
+describe("showResults", function() {
+	it("Should show the results", function(){
+		expect(0).toBe(0);
+	});
+});
+
